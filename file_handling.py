52c679e313936e960b1deb92c021482b5aebbf58
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2025-09-17 20:18:49 +0100

    setup: file handling with a default file extention

--- file_handling.py
+++ file_handling.py
@@ -1,11 +1,26 @@
-favorite_quote = input("enter favorite quote")
+import os
 
-file_to_save_favorite_quote = input("enter file to save favorite quote and end it with an extention")
+favorite_quote = input("enter favorite quote: ")
 
-open(file_to_save_favorite_quote,"x")
+file_to_save_favorite_quote = input("enter file to save favorite quote and end it with an extention: ")
 
-with open(file_to_save_favorite_quote,"a") as f:
-    f.write(favorite_quote)
 
-with open(file_to_save_favorite_quote) as f:
-    print(f.read())
+
+root, extention = os.path.splitext(file_to_save_favorite_quote)
+print(root, extention)
+
+if extention == "":
+    print("must end with an extention")
+elif extention != ".txt":
+    print(f"{file_to_save_favorite_quote} must end with a .txt extention")
+else:
+    open(root,"x")
+
+ 
+
+
+# with open(file_to_save_favorite_quote,"a") as f:
+#     f.write(favorite_quote)
+
+# with open(file_to_save_favorite_quote) as f:
+#     print(f.read())
