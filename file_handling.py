60054786fcafd788c6ba55f50967698cf9fb0ab4
Akinlade Temitope <akinlade_temitope0506@yahoo.com>
2025-09-17 20:52:59 +0100

    setup: nested conditions in a loop to enhance user's experience

--- file_handling.py
+++ file_handling.py
@@ -2,25 +2,22 @@ import os
 
 favorite_quote = input("enter favorite quote: ")
 
-file_to_save_favorite_quote = input("enter file to save favorite quote and end it with an extention: ")
+while True:
+    file_to_save_favorite_quote = input("enter file to save favorite quote and end it with a '.txt' extention: ")
+    root, extention = os.path.splitext(file_to_save_favorite_quote)
+    if extention == "":
+        print("must end with an extention")
+    elif extention != ".txt":
+        print(f"{file_to_save_favorite_quote} must end with a .txt extention")
+    else:
+        open(root+extention,"x")
+        with open(file_to_save_favorite_quote,"a") as f:
+            f.write(favorite_quote)
 
-
-
-root, extention = os.path.splitext(file_to_save_favorite_quote)
-print(root, extention)
-
-if extention == "":
-    print("must end with an extention")
-elif extention != ".txt":
-    print(f"{file_to_save_favorite_quote} must end with a .txt extention")
-else:
-    open(root,"x")
+        with open(file_to_save_favorite_quote) as f:
+            print(f.read())
+        break
 
  
 
 
-# with open(file_to_save_favorite_quote,"a") as f:
-#     f.write(favorite_quote)
-
-# with open(file_to_save_favorite_quote) as f:
-#     print(f.read())
